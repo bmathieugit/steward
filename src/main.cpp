836ea4bf0024dd4a7a24c5bdaf8826bcdf977706
bmathieugit <bmathieugit@gmail.com>
2022-09-05 17:22:43 +0200

    ansi poc

--- src/main.cpp
+++ src/main.cpp
@@ -5,44 +5,54 @@
 
 #include <string>
 #include <vector>
+#include <bit>
 
+#include <array>
+#include <iostream>
 
 int main(int argc, char **argv)
 {
-  stew::ui::text_input ti_login("login");
-  stew::ui::text_input ti_passw("password");
+  /*
+  std::string s;
+  std::cout << "login : "<<'\n';
+  std::cout << "mdp   : "<<'\n';
+  std::cout << "\033[2A";
+  std::cout << "\033[8C";
+  std::cin >> s;
+  std::cout << "\033[8C";
+  std::cin >> s;
+ */
+  std::array<std::array<char, 80>, 60> buffer;
 
-  std::string login;
-  std::string password;
+  for (std::size_t i(0); i < buffer.size(); ++i)
+    for (std::size_t j(0); j < buffer[i].size(); ++j)
+      buffer[i][j] = ' ';
 
-  ti_login.render();
-  ti_login.capture(login);
-  ti_passw.render();
-  ti_passw.capture(password);
-  
-  std::cout << "login : " << login << "; password : " << password << "\n";
-
-  stew::ui::table table({"login", "password"});
-  
-  table.push_row({"bmathieugit", "PassWORD!!!"});
-  table.push_row({"bmathieugit52", "AzertyUi"});
-  table.push_row({"bmathieugit_alter", "PassWORD!!!-6q5s4d65q4sd54M*qsdd"});
-
-  table.render();
-
-  /* if (argc > 2)
+  for (std::size_t k(0); k < 10; ++k)
   {
-    std::vector<std::string> args(argv + 1, argv + argc);
+    std::string m;
+    buffer[59][79] = '0' + k;
+    for (std::size_t i(0); i < buffer.size(); ++i)
+      for (std::size_t j(0); j < buffer[i].size(); ++j)
+        std::cout << buffer[i][j];
+    
+    std::cin >> m;
+    std::cout << "\033[2J";
+    std::cout << "\033[H";
+  }
+  /* if (argc > 2)
+ {
+   std::vector<std::string> args(argv + 1, argv + argc);
 
-    if (args.size() >= 2)
-    {
-      if (args[0] == "vault")
-      {
-        stew::vault_agent<stew::cesar_crypto_algorithm> vaulta;
-        vaulta.process(args);
-      }
-    }
-  } */
+   if (args.size() >= 2)
+   {
+     if (args[0] == "vault")
+     {
+       stew::vault_agent<stew::cesar_crypto_algorithm> vaulta;
+       vaulta.process(args);
+     }
+   }
+ } */
 
   return EXIT_SUCCESS;
 }
