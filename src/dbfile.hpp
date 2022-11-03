a1d521644a865327d8e7be0ad1cf17164967f7f4
bmathieugit <bmathieugit@gmail.com>
2022-11-03 11:38:45 +0100

    memory db

--- src/dbfile.hpp
+++ src/dbfile.hpp
@@ -37,149 +37,33 @@ namespace stew::variant
   constexpr int same_index_as = same_index_as_impl<T, V>();
 }
 
-#include <list>
+#include <any>
+#include <map>
 #include <string>
 #include <variant>
-#include <vector>
-
-namespace stew::dbf::storage::format
-{
-  struct array_tag
-  {
-  };
-
-  using number = double;
-  using string = std::string;
-  using boolean = bool;
-  using none = std::monostate;
-
-  using data = std::variant<
-      none, number, string, boolean>;
-
-  class tree
-  {
-    std::map<std::string, data> _data;
-    std::map<std::string, std::size_t> _arrays;
-
-  public:
-    ~tree() = default;
-    tree() = default;
-    tree(const tree &) = default;
-    tree(tree &&) = default;
-    tree &operator=(const tree &) = default;
-    tree &operator=(tree &&) = default;
-
-  public:
-    void size_of(std::string &&pth, std::size_t size)
-    {
-      _arrays[pth] = size;
-    }
-
-    void size_of(const std::string &pth, std::size_t size)
-    {
-      size_of(std::move(pth), size);
-    }
-
-    const std::size_t size_of(const std::string &pth) const
-    {
-      return _arrays.contains(pth) ? _arrays.at(pth) : 0;
-    }
-
-  public:
-    template <typename T>
-    std::optional<T> get(const std::string &pth) const
-    {
-      if (_data.contains(pth))
-      {
-        return std::optional<T>(std::get<T>(_data.at(pth)));
-      }
-
-      return std::nullopt;
-    }
-
-    template <typename T>
-    void set(std::string &&pth, const T &obj)
-    {
-      _data[pth] = obj;
-    }
-
-    template <typename T>
-    void set(const std::string &pth, const T &obj)
-    {
-      set(std::move(pth), obj);
-    }
-  };
-
-  template <typename T>
-  T unmarshall(tree &tr)
-  {
-    T t;
-    unmarshall(tr, t, "");
-    return t;
-  }
-
-  template <typename T>
-  tree marshall(const T &obj)
-  {
-    tree tr;
-    marshall(tr, obj, "");
-    return tr;
-  }
-
-  template<typename T>
-  void marshall(tree& t, const std::vector<T>& v, std::string_view parent_path)
-  {
-    for (int i=0; i<v.size(); ++i)
-    {
-      marshall(t, v[i], std::string(parent_path) + "/" + std::to_string(i));
-    }
-  }
-
-  template<typename T>
-  void unmarshall(const tree& t, std::vector<T>& v, std::string_view parent_path)
-  {
-    auto size = t.size_of(std::string(parent_path));
-
-    for (int i = 0; i < size; ++i)
-    {
-      T tmp;
-      unmarshall(t, tmp, std::string(parent_path) + "/" + std::to_string(i));
-      v.push_back(tmp);
-    }
-  }
-
-  template<typename T>
-  void marshall(tree& t, const std::list<T>& l, std::string_view parent_path)
-  {
-    int i=0;
-    for (const auto& item : l)
-    {
-      marshall(t, l, std::string(parent_path) + "/" + std::to_string(i));
-      ++i;
-    }
-  }
-}
 
 namespace stew::dbf::storage::mem
 {
-  template <typename T>
   class leaf
   {
     std::string _name;
-    T _data;
-
-  public:
-    using stored_type = T;
+    std::any _data;
 
   public:
     ~leaf() = default;
     leaf() = default;
+    leaf(const std::string &name) : _name(name) {}
+    leaf(std::string &&name) : _name(name) {}
+    template <typename T>
     leaf(const std::string &name, const T &data)
         : _name(name), _data(data) {}
-    leaf(const std::string &name, T &&data)
-        : _name(name), _data(data) {}
+    template <typename T>
     leaf(std::string &&name, const T &data)
         : _name(name), _data(data) {}
+    template <typename T>
+    leaf(const std::string &name, T &&data)
+        : _name(name), _data(data) {}
+    template <typename T>
     leaf(std::string &&name, T &&data)
         : _name(name), _data(data) {}
     leaf(const leaf &) = default;
@@ -193,50 +77,40 @@ namespace stew::dbf::storage::mem
       return _name;
     }
 
-    const T &data() const
+    void name(const std::string &n)
+    {
+      _name = n;
+    }
+
+    void name(std::string &&n)
+    {
+      _name = n;
+    }
+
+    const std::any &data() const
     {
       return _data;
     }
 
-    T &data()
+    template <typename T>
+    void data(T &&data)
     {
-      return _data;
+      _data = data;
     }
 
-    void data(const T &d)
+    template <typename T>
+    void data(const T &data)
     {
-      _data = d;
-    }
-
-    void data(T &&d)
-    {
-      _data = d;
+      _data = data;
     }
   };
 
-  template <typename T>
-  class root;
-
-  template <typename T>
-  using node = root<T>;
-
-  template <typename T>
-  using child = std::variant<leaf<T>, node<T>>;
-
-  template <typename T, typename O>
-  constexpr bool instance_of(const child<O> &c)
-  {
-    return stew::variant::same_index_as<T, child<O>> == c.index();
-  }
-
-  template <typename T>
   class root
   {
-    std::string _name;
-    std::map<std::string, child<T>> _childs;
+    using node = std::variant<root, leaf>;
 
-  public:
-    using stored_type = T;
+    std::string _name;
+    std::map<std::string, node> _childs;
 
   public:
     ~root() = default;
@@ -260,42 +134,42 @@ namespace stew::dbf::storage::mem
     }
 
   public:
-    template <typename... S>
+    template <typename T, typename... S>
     bool insert(T &&data, const std::string &pth, const S &...s)
     {
       if (_childs.contains(pth))
       {
         if constexpr (sizeof...(s) == 0)
         {
-          if (instance_of<leaf<T>>(_childs[pth]))
+          if (_childs[pth].index() == 1)
           {
-            std::get<leaf<T>>(_childs[pth]).data(data);
+            std::get<leaf>(_childs[pth]).data(data);
             return true;
           }
         }
         else
         {
-          return std::get<node<T>>(_childs[pth]).insert(data, s...);
+          return std::get<root>(_childs[pth]).insert(data, s...);
         }
       }
       else
       {
         if constexpr (sizeof...(s) == 0)
         {
-          _childs[pth] = leaf<T>(pth, data);
+          _childs[pth] = leaf(pth, data);
           return true;
         }
         else
         {
-          _childs[pth] = node<T>(pth);
-          return std::get<node<T>>(_childs[pth]).insert(data, s...);
+          _childs[pth] = root(pth);
+          return std::get<root>(_childs[pth]).insert(data, s...);
         }
       }
 
       return false;
     }
 
-    template <typename... S>
+    template <typename T, typename... S>
     bool insert(const T &data, const std::string &pth, S... s)
     {
       T copy = data;
@@ -314,9 +188,9 @@ namespace stew::dbf::storage::mem
         }
         else
         {
-          if (instance_of<root<T>>(_childs[pth]))
+          if (_childs[pth].index() == 0)
           {
-            return std::get<root<T>>(_childs[pth]).remove(s...);
+            return std::get<root>(_childs[pth]).remove(s...);
           }
         }
       }
@@ -324,18 +198,6 @@ namespace stew::dbf::storage::mem
       return false;
     }
   };
-
-  template <typename T>
-  root<T> make_root(const std::string &name)
-  {
-    return root<T>{name};
-  }
-
-  template <typename T>
-  root<T> make_root(std::string &&name)
-  {
-    return root<T>{std::move(name)};
-  }
 }
 
 namespace stew::dbf::storage::api
@@ -356,14 +218,14 @@ namespace stew::dbf::storage::api
     basic_storage &operator=(basic_storage &&) = default;
 
   public:
-    template <typename... S>
-    bool insert(const STORAGE::stored_type &data, const std::string &pth, const S &...s)
+    template <typename T,typename... S>
+    bool insert(const T &data, const std::string &pth, const S &...s)
     {
       return _storage.insert(data, pth, s...);
     }
 
-    template <typename... S>
-    bool insert(STORAGE::stored_type &&data, const std::string &pth, const S &...s)
+    template <typename T, typename... S>
+    bool insert(T &&data, const std::string &pth, const S &...s)
     {
       return _storage.insert(data, pth, s...);
     }
@@ -375,8 +237,7 @@ namespace stew::dbf::storage::api
     }
   };
 
-  template <typename T>
-  using memory = basic_storage<stew::dbf::storage::mem::root<T>>;
+  using memory = basic_storage<stew::dbf::storage::mem::root>;
 
   // template <typename T>
   // using fs = basic_storage<stew::dbf::storage::fs::dir<T>>;
--- src/main.cpp
+++ src/main.cpp
@@ -6,68 +6,21 @@
 #include <filesystem>
 #include <iostream>
 
-template <typename T>
-void printdb(const stew::dbf::storage::mem::node<T> &n)
+void printdb(const stew::dbf::storage::mem::root &r)
 {
-  std::cout << n.name() << '\n';
+  std::cout << r.name() << '\n';
 
-  for (const auto &[_, i] : n.childs())
+  for (const auto &[_, i] : r.childs())
   {
-    if (stew::dbf::storage::mem::instance_of<stew::dbf::storage::mem::node<T>>(i))
+    if (i.index() == 0)
     {
-      printdb(std::get<stew::dbf::storage::mem::node<T>>(i));
+      printdb(std::get<stew::dbf::storage::mem::root>(i));
     }
     else
     {
-      std::cout << std::get<stew::dbf::storage::mem::leaf<T>>(i).name()
-                << " : " << std::get<stew::dbf::storage::mem::leaf<T>>(i).data() << '\n';
+      std::cout << std::get<stew::dbf::storage::mem::leaf>(i).name()
+                << " : " << std::any_cast<std::string>(std::get<stew::dbf::storage::mem::leaf>(i).data()) << '\n';
     }
-  }
-}
-
-struct address
-{
-  std::string street;
-  int num;
-};
-
-struct person
-{
-  std::string name;
-  std::string firstname;
-  std::vector<address> addrs;
-};
-
-namespace stew::dbf::storage::format
-{
-
-  void unmarshall(const tree &t, address &a, std::string_view parent_path)
-  {
-    a.num = (int)t.get<number>(std::string(parent_path) + "/num").value();
-    a.street = t.get<string>(std::string(parent_path) + "/street").value();
-  }
-
-  void unmarshall(const tree &t, person &p, std::string_view parent_path)
-  {
-    unmarshall(t, p.addrs, std::string(parent_path) + "/address");
-    p.name = t.get<string>(std::string(parent_path) + "/name").value();
-    p.firstname = t.get<string>(std::string(parent_path) + "/firstname").value();
-  }
-
-  void marshall(tree &t, const address &a, std::string_view parent_path)
-  {
-    t.set(std::string(parent_path) + "/street", a.street);
-    t.set(std::string(parent_path) + "/num", (number)a.num);
-  }
-
-  void marshall(tree &t, const person &p, std::string_view parent_path)
-  {
-    t.set(std::string(parent_path) + "/name", p.name);
-    t.set(std::string(parent_path) + "/firstname", p.firstname);
-
-    marshall(t, p.addrs, std::string(parent_path) + "/address");
-
-    t.size_of( std::string(parent_path) + "/address", p.addrs.size());
   }
 }
 
@@ -75,29 +28,21 @@ int main()
 {
   using namespace std::literals::string_literals;
 
-  stew::dbf::storage::mem::root<std::string> r = stew::dbf::storage::mem::make_root<std::string>("ldap");
+  stew::dbf::storage::mem::root r("ldap");
 
-  stew::dbf::storage::api::memory<std::string> mem("ldap");
+  stew::dbf::storage::api::memory mem("ldap");
   std::cout << std::boolalpha << mem.insert("martin"s, "people", "name");
   std::cout << std::boolalpha << mem.insert("martin"s, "people", "name2");
   std::cout << std::boolalpha << mem.insert("martin"s, "people", "name3");
   std::cout << std::boolalpha << mem.insert("martin"s, "people", "name4");
   std::cout << std::boolalpha << mem.insert("martin"s, "people", "name5");
 
-  std::optional<std::string> res = stew::dbf::query::select(mem, "people", "name");
-  bool res2 = stew::dbf::query::insert(mem, "martin"s, "people", "name2");
-  bool res3 = stew::dbf::query::update(mem, "martin"s, "people", "name2");
-  bool res4 = stew::dbf::query::remove(mem, "people", "name2");
-  std::size_t cnt = stew::dbf::query::count(mem, "people", "name2");
-  bool ex = stew::dbf::query::exists(mem, "people", "name2");
-
-  stew::dbf::storage::format::tree tr;
-
-  person p{"smith", "john", {{"cross road stree", 12}}};
-
-  stew::dbf::storage::format::tree pt = stew::dbf::storage::format::marshall(p);
-  person p2 = stew::dbf::storage::format::unmarshall<person>(pt);
-  std::cout << p2.name << p2.firstname << p2.addrs[0].num << p2.addrs[0].street;
+  // std::optional<std::string> res = stew::dbf::query::select(mem, "people", "name");
+  // bool res2 = stew::dbf::query::insert(mem, "martin"s, "people", "name2");
+  // bool res3 = stew::dbf::query::update(mem, "martin"s, "people", "name2");
+  // bool res4 = stew::dbf::query::remove(mem, "people", "name2");
+  // std::size_t cnt = stew::dbf::query::count(mem, "people", "name2");
+  // bool ex = stew::dbf::query::exists(mem, "people", "name2");
 
   return 0;
 }
