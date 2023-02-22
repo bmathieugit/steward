477effc69a512ea58d483d414af521ce79bb59da
bmathieugit <bmathieugit@gmail.com>
2023-02-22 16:48:01 +0100

    add encode and decode hex

--- src/main.cpp
+++ src/main.cpp
@@ -4,61 +4,56 @@
 
 using namespace stew;
 
-int main()
+
+
+string<char> encode_hex(const string<char>& in)
 {
-  // bien passons au chose serieuse. 
-  // Nous alloons maintenant mettre en place un petit 
-  // prototype de ce que sera l'outil steward en ligne 
-  // de commande. Il devra permettre de sauver des mots
-  // de passe et de les redonner sur demande. 
-  // Il nous faut donc un algorithm de chiffrement et
-  // de déchiffrement pour les stocker de manière la plus
-  // sécurisée possible. 
-  // Cette algorithme sera à bijectif sur la base d'un 
-  // mot de passe maitre. 
-  //
-  //
+  constexpr string_view<char> encode_table = "0123456789abcdef";
+  string<char> out(in.size()*2+10);
 
-
-  static_string<char, 10> mdp = "password"_sv;
-  static_string<char, 100> master = "master"_sv;
-
-  // J'ai deux string l'une qui est le mdp a chiffer et l'autre 
-  // qui est le mot de passe maitre clé du chiffrement.
-  //
-
-  // On va faire un chiffrement simlement pour commencer. 
-  // On va prendre simplement chaque lettre du mot de passe maitre
-  // et additionner ce caracter avec le caractère à chiffrer dans 
-  // le mot de passe fils.
-  //
-
-  vector<byte_t> mdpb = mdp;
-  vector<byte_t> masterb = master;
-  vector<byte_t> result;
-
-  for (byte_t b : mdpb)
+  for (char c : in)
   {
-    size_t l = b;
-
-    for (byte_t m : masterb)
-    {
-      l += m;
-    }
-
-    for (byte_t m : *reinterpret_cast<array<byte_t, sizeof(size_t)>*>(&l))
-    {
-      result.push(m);
-    }
+    out.push(encode_table[c >> 4]);
+    out.push(encode_table[c & 15]);
   }
+
+  return out;
+}
+
+constexpr char reducehex(char c)
+{
+  return (c - '0') <= ('9' - '0') ? c - '0' : c - 'a' + 10;
+}
+
+char fromhex(char c0, char c1)
+{
+  return reducehex(c0) << 4 | reducehex(c1);
+}
+
+string<char> decode_hex(const string<char>& in)
+{
+  assert(in.size() % 2 == 0);
+  string<char> out(in.size()/2+10);
+
+  auto b = in.begin();
+  auto e = in.end();
+
+  while (b != e)
+  { 
+    char c0 = *b++;
+    char c1 = *b++;
+    out.push(fromhex(c0, c1));
+  }
+
+  return out;
+}
+
+
+int main(int argc, char** argv)
+{
+  console<char>::printfln("encode hex \0", encode_hex("Hello World"_sv));
+  console<char>::printfln("decode hex \0", decode_hex(encode_hex("Hello World"_sv)));
   
-  string<char> s = result;
-
-
-  console<char>::printfln("result is '\0'", s);
-
-  
-
   return 0;
 }
 
--- src/stew.hpp
+++ src/stew.hpp
@@ -5286,6 +5286,80 @@ namespace stew
 
   //------------------------
   //
+  // BASE64 format
+  //
+  //------------------------
+ 
+  template<typename T>
+  class base64
+  {
+  private:
+    universal_reference<T> _ref;
+
+  public:
+    constexpr base64(T&& ref) : _ref(ref) {}
+    constexpr base64(const T& ref) : _ref(ref) {}
+
+  public:
+    auto get() -> decltype(auto)
+    {
+      return _ref.get();
+    }
+
+    auto get() const -> decltype(auto)
+    {
+      return _ref.get();
+    }
+  };
+/*
+  template<range R>
+  class formatter<base64<R>>
+  {
+  public:
+    template<ostream O>
+    constexpr static void to(O &o, const base64<R> &b)
+    {
+      static constexpr char encoding_table[] = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w', 'x', 'y', 'z', '0', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9', '+', '/'
+      };
+
+      size_t in_len = b.get().size();
+      size_t out_len = 4 * ((in_len + 2) / 3);
+      size_t i;
+
+      char *p = const_cast<char*>(ret.c_str());
+
+      for (i = 0; i < in_len - 2; i += 3) {
+        *p++ = sEncodingTable[(data[i] >> 2) & 0x3F];
+        *p++ = sEncodingTable[((data[i] & 0x3) << 4) | ((int) (data[i + 1] & 0xF0) >> 4)];
+        *p++ = sEncodingTable[((data[i + 1] & 0xF) << 2) | ((int) (data[i + 2] & 0xC0) >> 6)];
+        *p++ = sEncodingTable[data[i + 2] & 0x3F];
+      }
+      if (i < in_len) {
+        *p++ = sEncodingTable[(data[i] >> 2) & 0x3F];
+        if (i == (in_len - 1)) {
+          *p++ = sEncodingTable[((data[i] & 0x3) << 4)];
+          *p++ = '=';
+        }
+        else {
+          *p++ = sEncodingTable[((data[i] & 0x3) << 4) | ((int) (data[i + 1] & 0xF0) >> 4)];
+          *p++ = sEncodingTable[((data[i + 1] & 0xF) << 2)];
+        }
+        *p++ = '=';
+      }
+    }
+  }
+  
+*/
+  //------------------------
+  //
   // XML format
   //
   //------------------------
