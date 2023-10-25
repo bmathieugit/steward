2b0ed7bedd44f379e5b76d695ac086ee6fc273ed
bmathieugit <bmathieugit@gmail.com>
2023-10-25 05:56:17 +0200

    amélioration du framework de test

--- test/test-string.cpp
+++ test/test-string.cpp
@@ -1,74 +1,74 @@
 #include <core/string.hpp>
 #include <tests.hpp>
 template <size_t N>
-bool operator==(const stringlike auto& s1, const char* s2) {
+constexpr bool operator==(const stringlike auto& s1, const char* s2) {
   auto is1 = istream(s1);
   auto is2 = istream(s2);
   return equals(is1, is2);
 }
 
-void test_string_create() {
+constexpr void test_string_create() {
   string s = istream("Hello");
   N_TEST_ASSERT_EQUALS(s.len(), 5);
 }
 
-void test_string_copy_constructor() {
+constexpr void test_string_copy_constructor() {
   string s = istream("Hello");
   string s2 = s;
   ::printf("%lu", s2.len());
   N_TEST_ASSERT_EQUALS(s2.len(), 5);
 }
 
-void test_string_move_constructor() {
+constexpr void test_string_move_constructor() {
   string s = istream("Hello");
   string s3 = move(s);
   N_TEST_ASSERT_EQUALS(s3.len(), 5);
 }
 
-void test_string_copy_assignment() {
+constexpr void test_string_copy_assignment() {
   string s = istream("Hello");
   string s2;
   s2 = s;
   N_TEST_ASSERT_EQUALS(s2.len(), 5);
 }
 
-void test_string_move_assignment() {
+constexpr void test_string_move_assignment() {
   string s = istream("Hello");
   string s3;
   s3 = move(s);
   N_TEST_ASSERT_EQUALS(s3.len(), 5);
 }
 
-void test_string_len() {
+constexpr void test_string_len() {
   string s = istream("Hello");
   N_TEST_ASSERT_EQUALS(s.len(), 5);
 }
 
-void test_string_empty() {
+constexpr void test_string_empty() {
   string s;
   N_TEST_ASSERT_TRUE(s.empty());
 }
 
-void test_string_full() {
+constexpr void test_string_full() {
   string s = istream("Hello");
   N_TEST_ASSERT_FALSE(s.full());
 }
 
-void test_string_add() {
+constexpr void test_string_add() {
   string s;
   s.add('H');
   s.add('i');
   N_TEST_ASSERT_EQUALS(s.len(), 2);
 }
 
-void test_string_remove() {
+constexpr void test_string_remove() {
   string s = istream("Hi");
   s.remove(typename string::position{s.len() - 1});
   N_TEST_ASSERT_EQUALS(s.len(), 1);
   N_TEST_ASSERT_FALSE(s.full());
 }
 
-void test_string_clear() {
+constexpr void test_string_clear() {
   string s = istream("Clear me");
   s.clear();
   N_TEST_ASSERT_TRUE(s.empty());
--- test/tests.hpp
+++ test/tests.hpp
@@ -1,8 +1,10 @@
 #ifndef __n_tests_hpp__
 #define __n_tests_hpp__
 
-#include <stdio.h>
-#include <string.h>
+#include <core/array.hpp>
+#include <core/char-ostream.hpp>
+#include <core/io.hpp>
+#include <core/string.hpp>
 
 #define N_TEST_ASSERT_TRUE(condition)    \
   if (!(condition)) {                    \
@@ -23,7 +25,7 @@
   {                        \
     test_suite suite(name);
 
-#define N_TEST_REGISTER(t) suite.push(test(#t, (t)));
+#define N_TEST_REGISTER(t) suite.add(test(#t, (t)));
 
 #define N_TEST_RUN_SUITE \
   suite.run_all();       \
@@ -56,67 +58,14 @@ class test {
     _func();
     return test_result::success;
   } catch (const char* e) {
-    ::printf("\033[1;31merror : %s\033[0m\n", e);
+    sout << "\033[1;31merror : " << e << "\033[0m\n";
     return test_result::failure;
   }
 };
 
-class test_vector {
- private:
-  test* _tests = nullptr;
-  int _size = 0;
-  int _max = 0;
-
- public:
-  constexpr ~test_vector() {
-    if (_tests != nullptr)
-      delete[] _tests;
-    _size = 0;
-    _max = 0;
-  }
-
-  constexpr test_vector(int max) : _tests(new test[max]), _size(0), _max(max) {}
-
-  // copy
-  constexpr test_vector(const test_vector&) = delete;
-  constexpr test_vector& operator=(const test_vector&) = delete;
-
-  // move
-  constexpr test_vector(test_vector&&) = default;
-  constexpr test_vector& operator=(test_vector&&) = default;
-
-  constexpr int size() const { return _size; }
-  constexpr bool empty() const { return _size == 0; }
-  constexpr int max() const { return _max; }
-
-  constexpr void push(const test& t) {
-    if (_size != _max) {
-      _tests[_size] = t;
-      ++_size;
-    }
-  }
-
-  constexpr test& at(int i) { return _tests[i]; }
-};
-
-namespace str {
-
-template <int N>
-void padln(char (&buffer)[N], char c) {
-  int len = ::strlen(buffer);
-
-  for (int i = len; i < N - 1; ++i) {
-    buffer[i] = c;
-  }
-
-  buffer[N - 2] = '\n';
-  buffer[N - 1] = '\0';
-}
-}  // namespace str
-
 class test_suite {
  private:
-  test_vector _tests{100};
+  array<test, 100> _tests;
   const char* _name = nullptr;
 
  public:
@@ -124,41 +73,35 @@ class test_suite {
   constexpr test_suite(const char* name) : _name(name) {}
 
  public:
-  constexpr void push(const test& t) { _tests.push(t); }
+  constexpr void add(const test& t) { _tests.add(t); }
 
  public:
-  void run_all() {
-    constexpr int MAX = 80;
-    char buffer[MAX];
-
+  constexpr void run_all() {
     int succeed = 0;
     int failed = 0;
 
-    ::snprintf(buffer, MAX - 1, "\nSUITE [%s] ", _name);
-    str::padln(buffer, '*');
-    ::printf("%s", buffer);
+    sout << "\nSUITE [" << _name << "] ";
 
-    for (int i = 0; i < _tests.size(); ++i) {
-      ::snprintf(buffer, MAX - 1, "\nTEST [%s] ", _tests.at(i).name());
-      str::padln(buffer, '*');
-      ::printf("%s", buffer);
-      test_result res = _tests.at(i).run();
+    auto tests_os = istream(_tests);
+    
+    while (tests_os.has()) {
+      auto&& t = tests_os.next();
+      sout << "\nTEST[" << t.name() << "]\n";
+      auto res = t.run();
 
       if (res == test_result::success) {
         ++succeed;
-        ::printf(" - Result : \033[1;32mPASSED\033[0m\n");
+        sout << " - Result : \033[1;32mPASSED\033[0m\n";
       } else {
         ++failed;
-        ::printf(" - Result : \033[1;31mFAILED\033[0m\n");
+        sout << " - Result : \033[1;31mFAILED\033[0m\n";
       }
     }
-    ::snprintf(buffer, MAX - 1, "\nSUITE RECAP ");
-    str::padln(buffer, '*');
-    ::printf("%s", buffer);
-    ::printf(" - Passed : \033[1;32m%d\033[0m\n", succeed);
-    ::printf(" - Failed : \033[1;31m%d\033[0m\n", failed);
 
-    ::printf("\n");
+    sout << "\nSUITE RECAP ";
+    sout << " - Passed : \033[1;32m" << succeed << "\033[0m\n";
+    sout << " - Failed : \033[1;31m" << failed << "\033[0m\n";
+    sout << '\n';
   }
 };
 
