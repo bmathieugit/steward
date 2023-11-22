3840361d492c6f5737987fec30320daf8e2ea3f4
bmathieugit <bmathieugit@gmail.com>
2023-11-22 09:11:12 +0100

    ajout constexpr pour les fonctions de to_hash

--- src/core/hash.hpp
+++ src/core/hash.hpp
@@ -22,12 +22,12 @@ template <>
 constexpr size_t fnvoffset<64> = 14695981039346656037UL;
 
 template <size_t N>
-uof<N> to_hash(byte_t b, uof<N> h = fnvoffset<N>) {
+constexpr uof<N> to_hash(byte_t b, uof<N> h = fnvoffset<N>) {
   return (h ^ b) * fnvprime<N>;
 }
 
 template <size_t N, size_t M>
-uof<N> to_hash(const byte_t (&bs)[M], uof<N> h = fnvoffset<N>) {
+constexpr uof<N> to_hash(const byte_t (&bs)[M], uof<N> h = fnvoffset<N>) {
   for (size_t i = 0; i < M; ++i) {
     h = to_hash<N>(bs[i], h);
   }
@@ -36,17 +36,17 @@ uof<N> to_hash(const byte_t (&bs)[M], uof<N> h = fnvoffset<N>) {
 }
 
 template <size_t N>
-uof<N> to_hash(wchar_t c, uof<N> h = fnvoffset<N>) {
+constexpr uof<N> to_hash(wchar_t c, uof<N> h = fnvoffset<N>) {
   return to_hash<N>(static_cast<const byte_t[sizeof(wchar_t)]>(c), h);
 }
 
 template <size_t N, integral I>
-uof<N> to_hash(const I i, uof<N> h = fnvoffset<N>) {
+constexpr uof<N> to_hash(const I i, uof<N> h = fnvoffset<N>) {
   return to_hash<N>(static_cast<const byte_t[sizeof(I)]>(i), h);
 }
 
 template <size_t N, iterator I>
-uof<N> to_hash(I i, uof<N> h = fnvoffset<N>) {
+constexpr uof<N> to_hash(I i, uof<N> h = fnvoffset<N>) {
   while (i.has()) {
     h = to_hash<N>(i.next(), h);
   }
@@ -55,7 +55,7 @@ uof<N> to_hash(I i, uof<N> h = fnvoffset<N>) {
 }
 
 template <size_t N, collection C>
-uof<N> to_hash(const C& c, uof<N> h = fnvoffset<N>) {
+constexpr uof<N> to_hash(const C& c, uof<N> h = fnvoffset<N>) {
   return to_hash<N>(iter(c), h);
 }
 
